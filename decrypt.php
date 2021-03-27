b06c5e12788d62abfe722ec01b4cfc62a3baab61
Anna <anna.shesterikova@cdnnow.ru>
2021-03-27 17:27:32 +0300

    added custom alphabet number

--- decrypt.php
+++ decrypt.php
@@ -1,31 +1,29 @@
 <?php
 echo 'Производится расшифровка...' . PHP_EOL;
 
-if (empty($argv[1]) || empty($argv[2]) || empty($argv[3]) || empty($argv[4])) {
+if (empty($argv[1]) || empty($argv[2]) || empty($argv[3])) {
     exit('Недостаточно аргументов');
 }
 
 $string = $argv[1];
-$shift1 = (int)$argv[2];
-$shift2 = (int)$argv[3];
-$shift3 = (int)$argv[4];
-$limit = mb_strlen($string, 'UTF-8');
+$alphabetQuantity = (int)$argv[2];
+$shifts = explode(' ', $argv[3]);
+
+if (count($shifts) !== $alphabetQuantity) {
+    exit('Количество алфавитов не совпадает с количеством смещений');
+}
 
 $alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
-$cryptoAlphabet1 = mb_substr($alphabet, $shift1) . mb_substr($alphabet, 0, $shift1);
-$cryptoAlphabet2 = mb_substr($alphabet, $shift2) . mb_substr($alphabet, 0, $shift2);
-$cryptoAlphabet3 = mb_substr($alphabet, $shift3) . mb_substr($alphabet, 0, $shift3);
+foreach ($shifts as $index => $shift) {
+    ${"alphabet$index"} = mb_substr($alphabet, $shift) . mb_substr($alphabet, 0, $shift);
+}
 
 $result = '';
+$limit = mb_strlen($string, 'UTF-8');
 for ($i = 0; $i < $limit; $i++) {
     $letter = mb_substr($string, $i, 1);
-    if ($i % 3 === 0) {
-        $position = mb_strripos($cryptoAlphabet1, $letter);
-    } elseif ($i % 3 === 1) {
-        $position = mb_strripos($cryptoAlphabet2, $letter);
-    } else {
-        $position = mb_strripos($cryptoAlphabet3, $letter);
-    }
+    $j = $i % $alphabetQuantity;
+    $position = mb_strripos(${"alphabet$j"}, $letter);
     if ($position !== false) {
         $result .= mb_substr($alphabet, $position, 1);
     } else {
--- encrypt.php
+++ encrypt.php
@@ -1,34 +1,31 @@
 <?php
 echo 'Производится шифрование...' . PHP_EOL;
 
-if (empty($argv[1]) || empty($argv[2]) || empty($argv[3]) || empty($argv[4])) {
+if (empty($argv[1]) || empty($argv[2]) || empty($argv[3])) {
     exit('Недостаточно аргументов');
 }
 
 $string = $argv[1];
-$shift1 = (int)$argv[2];
-$shift2 = (int)$argv[3];
-$shift3 = (int)$argv[4];
-$limit = mb_strlen($string, 'UTF-8');
+$alphabetQuantity = (int)$argv[2];
+$shifts = explode(' ', $argv[3]);
+
+if (count($shifts) !== $alphabetQuantity) {
+    exit('Количество алфавитов не совпадает с количеством смещений');
+}
 
 $alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
-$cryptoAlphabet1 = mb_substr($alphabet, $shift1) . mb_substr($alphabet, 0, $shift1);
-$cryptoAlphabet2 = mb_substr($alphabet, $shift2) . mb_substr($alphabet, 0, $shift2);
-$cryptoAlphabet3 = mb_substr($alphabet, $shift3) . mb_substr($alphabet, 0, $shift3);
-
+foreach ($shifts as $index => $shift) {
+    ${"alphabet$index"} = mb_substr($alphabet, $shift) . mb_substr($alphabet, 0, $shift);
+}
 
 $result = '';
+$limit = mb_strlen($string, 'UTF-8');
 for ($i = 0; $i < $limit; $i++) {
     $letter = mb_substr($string, $i, 1);
     $position = mb_strripos($alphabet, $letter);
     if ($position !== false) {
-        if ($i % 3 === 0) {
-            $result .= mb_substr($cryptoAlphabet1, $position, 1);
-        } elseif ($i % 3 === 1) {
-            $result .= mb_substr($cryptoAlphabet2, $position, 1);
-        } else {
-            $result .= mb_substr($cryptoAlphabet3, $position, 1);
-        }
+        $j = $i % $alphabetQuantity;
+        $result .= mb_substr(${"alphabet$j"}, $position, 1);
     } else {
         $result .= mb_substr($string, $i, 1);
     }
